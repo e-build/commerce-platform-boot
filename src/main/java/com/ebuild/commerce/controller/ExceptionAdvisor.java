2f2ac8dd58b3e3e94b8086e046381fabf3115f9e
e-build <donggeol2@daum.net>
2021-12-22 21:53:54 +0900

    Feat : HTTP 요청 공통 예외 처리 추가

---
+++ src/main/java/com/ebuild/commerce/controller/ExceptionAdvisor.java
@@ -0,0 +1,29 @@
+package com.ebuild.commerce.controller;
+
+import com.ebuild.commerce.common.http.CommonResponse;
+import com.ebuild.commerce.exception.AlreadyExistsException;
+import com.ebuild.commerce.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+public class ExceptionAdvisor {
+
+    @ExceptionHandler(value = {
+        org.springframework.validation.BindException.class,
+        MethodArgumentNotValidException.class,
+        AlreadyExistsException.class,
+        NotFoundException.class
+        })
+    public ResponseEntity<CommonResponse> badRequest(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(CommonResponse.ERROR(e));
+    }
+
+}
