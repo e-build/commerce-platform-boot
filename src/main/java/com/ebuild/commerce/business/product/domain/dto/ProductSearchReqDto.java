5aad9c9c9495d8a187df22ff4ee5ab0eebc81a23
e-build <donggeol2@daum.net>
2021-12-24 22:06:41 +0900

    Feat : 상품 조회 기능 추가
    - 추후 검색 필드 동적으로 선택할 수 있도록 Querydsl로 적용 예정
    - 빌드스크립트에 Querydsl 의존성 추가 후 주석처리

--- src/main/java/com/ebuild/commerce/business/product/domain/dto/ProductSearchReqDto.java
+++ src/main/java/com/ebuild/commerce/business/product/domain/dto/ProductSearchReqDto.java
@@ -4,14 +4,21 @@ import com.ebuild.commerce.business.product.domain.common.ProductStatus;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 @Getter
 @Setter
-public class ProductQueryReqDto {
+public class ProductSearchReqDto {
+
+  private Long companyId;
+
+  private String name;
 
   @DateTimeFormat(iso = ISO.DATE)
   private LocalDate saleStartDate;
@@ -19,10 +26,16 @@ public class ProductQueryReqDto {
   @DateTimeFormat(iso = ISO.DATE)
   private LocalDate saleEndDate;
 
+//  @DateTimeFormat(pattern = "")
+//  private LocalDateTime createdAtAfter;
+//
+//  @DateTimeFormat(pattern = "")
+//  private LocalDateTime updatedAtAfter;
+
   private ProductStatus productStatus;
 
-  private Integer page;
+  private Pageable pageable;
 
-  private Integer pageSize;
+  private String[] columns;
 
 }
--- src/main/java/com/ebuild/commerce/business/product/repository/JpaProductRepository.java
+++ src/main/java/com/ebuild/commerce/business/product/repository/JpaProductRepository.java
@@ -2,12 +2,19 @@ package com.ebuild.commerce.business.product.repository;
 
 import com.ebuild.commerce.business.company.domain.Company;
 import com.ebuild.commerce.business.product.domain.entity.Product;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaProductRepository extends JpaRepository<Product, Long> {
 
-  public Optional<Product> findByCompanyAndName(Company company, String name);
+  Optional<Product> findByCompanyAndName(Company company, String name);
 
+  Page<Product> findAllByCompanyAndNameContaining(Company company, String name, Pageable pageable);
+
+  Page<Product> findAll(Specification<Product> spec, Pageable pageable);
 
 }
