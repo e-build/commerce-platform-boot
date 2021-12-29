9ae3d504dd20f50eb4e20c2f10de4f9c2553ddfc
e-build <donggeol2@daum.net>
2021-12-29 22:01:20 +0900

    Refactor : Address 클래스 zipcode 필드명 변경

--- src/main/java/com/ebuild/commerce/common/Address.java
+++ src/main/java/com/ebuild/commerce/common/Address.java
@@ -2,21 +2,24 @@ package com.ebuild.commerce.common;
 
 import javax.persistence.Embeddable;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
   private String baseAddress;
   private String detailAddress;
-  private String addressZipAddress;
+  private String addressZipCode;
 
-  public Address(String baseAddress, String detailAddress, String addressZipAddress) {
+  @Builder
+  public Address(String baseAddress, String detailAddress, String addressZipCode) {
     this.baseAddress = baseAddress;
     this.detailAddress = detailAddress;
-    this.addressZipAddress = addressZipAddress;
+    this.addressZipCode = addressZipCode;
   }
+
 }
