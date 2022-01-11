6628f0f47288ea2e5264c11860190d9d60e11f0e
e-build <donggeol2@daum.net>
2022-01-11 17:29:28 +0900

    Feat : 로그인 시 응답 본무에 리프레시 토큰 포함
    - 리프레시 토큰 저장을 위한 레디스 키 상수 추가

--- src/main/java/com/ebuild/commerce/config/security/SecurityConstants.java
+++ src/main/java/com/ebuild/commerce/config/security/SecurityConstants.java
@@ -6,4 +6,6 @@ public class SecurityConstants {
   public static final String REFRESH_TOKEN_HEADER = "X-Refresh-Commerce-Token";
   public static final String AUTHORITIES_KEY = "ROLE";
 
+  public static final String REDIS_REFRESH_TOKEN_KEY = "REFRESH_TOKEN : ";
+
 }
