1e509f96f6911606fb15fa7d4492a85cc1de987f
e-build <donggeol2@daum.net>
2022-03-01 12:40:39 +0900

    Fix : 웹 정적 리소스 접근 허용.

--- src/main/java/com/ebuild/commerce/config/security/SecurityConfig.java
+++ src/main/java/com/ebuild/commerce/config/security/SecurityConfig.java
@@ -15,12 +15,14 @@ import com.ebuild.commerce.oauth.service.SecurityUserDetailsService;
 import com.ebuild.commerce.oauth.token.JWTProvider;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -53,6 +55,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+            .requestMatchers(PathRequest
+                .toStaticResources()
+                .atCommonLocations());
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .cors()
@@ -69,9 +79,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .authorizeRequests()
                     .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-//                .antMatchers("/api/v1/auth/**", "/oauth/redirect").permitAll()
-//                .antMatchers("/api/**").hasAnyAuthority(RoleType.USER.getCode())
-                .antMatchers("/api/**").permitAll()
+                .antMatchers("/api/v1/auth/**", "/oauth/redirect").permitAll()
+                .antMatchers("/api/**").hasAnyAuthority(RoleType.USER.getCode())
                 .antMatchers("/api/**/admin/**").hasAnyAuthority(RoleType.ADMIN.getCode())
                 .anyRequest().authenticated()
             .and()
